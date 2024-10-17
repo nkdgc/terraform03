804fcf4ddaf426aa052b9914b4eaec90533e7cab
nkdgc <deguchi0126@gmail.com>
2024-10-17 09:30:07 +0900

    allow 22/tcp

--- modules/web-server/main.tf
+++ modules/web-server/main.tf
@@ -35,6 +35,12 @@ resource "aws_security_group" "ubuntu-web-server-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     # security_groups = [aws_security_group.ubuntu-web-server-alb-sg.id]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
