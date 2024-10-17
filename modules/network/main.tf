41064dd712e8f4793d22fb41ae71b3135be5d831
nkdgc <deguchi0126@gmail.com>
2024-10-17 15:48:10 +0900

    added public-subnet01

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -9,3 +9,23 @@ resource "aws_vpc" "vpc01-vpc" {
     "Name" = "${var.name}"
   }
 }
+
+resource "aws_subnet" "public-subnet01" {
+  vpc_id = aws_vpc.vpc01-vpc.id
+  assign_ipv6_address_on_creation                = false
+  availability_zone                              = "ap-northeast-1a"
+  cidr_block                                     = "10.0.0.0/20"
+  customer_owned_ipv4_pool                       = null
+  enable_dns64                                   = false
+  enable_resource_name_dns_a_record_on_launch    = false
+  enable_resource_name_dns_aaaa_record_on_launch = false
+  ipv6_cidr_block                                = null
+  ipv6_cidr_block_association_id                 = null
+  ipv6_native                                    = false
+  map_public_ip_on_launch                        = false
+  outpost_arn                                    = null
+  private_dns_hostname_type_on_launch            = "ip-name"
+  tags                                           = {
+      "Name" = "vpc01-subnet-public1-ap-northeast-1a"
+  }
+}
