08c8c692a70acfa48f09ae0a1d75efded41a77b1
nkdgc <deguchi0126@gmail.com>
2024-10-17 10:30:44 +0900

    add envs/prd

---
+++ envs/prd/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source      = "../../modules/vpc"
+  name-prefix = local.name-prefix
+}
+
+module "web-server" {
+  source      = "../../modules/web-server"
+  name-prefix = local.name-prefix
+  subnet-id   = module.vpc.private-subnet01-id
+  vpc-id      = module.vpc.vpc-id
+}
---
+++ envs/prd/version.tf
@@ -0,0 +1,11 @@
+# Requirements
+terraform {
+  required_version = "~> 1.9.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.71"
+    }
+  }
+}
